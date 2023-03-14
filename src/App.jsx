09beb001f0104f13a4b1ecd0921e6a06af665598
zikaloai71 <ze.lo.moukhtar@el-eng.menofia.edu.eg>
2023-03-14 03:18:34 +0200

    add react router and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-
-import './App.css'
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
+import{BrowserRouter , Routes , Route} from "react-router-dom"
 
 function App() {
-  
   return (
-    <div>
-      Hello
-    </div>
+    <BrowserRouter>
+     <Header />
+      <Routes>
+        <Route path="/" element={<Photos />}/>
+        <Route path="/cart" element={<Cart/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'remixicon/fonts/remixicon.css'
+import{NavLink} from "react-router-dom"
+export default function Header() {
+  return (
+    <header>
+            <h2><NavLink  exact to="/">Pic Some</NavLink></h2>
+            <NavLink  to="/cart"><i class="ri-shopping-cart-line"></i></NavLink>
+    </header>
+  )
+}
